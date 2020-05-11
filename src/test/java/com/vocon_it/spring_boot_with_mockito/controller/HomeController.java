a503f12e87ddd1320cc4e04466d5b05b47fc5ca8
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-05-11 16:41:58 +0000

    cleaning

--- src/test/java/com/vocon_it/spring_boot_with_mockito/controller/HomeController.java
+++ src/test/java/com/vocon_it/spring_boot_with_mockito/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.vocon_it.spring_boot_with_mockito.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
