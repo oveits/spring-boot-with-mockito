58b8b288d139b726cc77678ccc9b58b324fd283f
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-05-11 15:48:29 +0000

    Added project up to MockMvc tests (WIP)

---
+++ src/test/java/com/vocon_it/spring_boot_with_mockito/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.vocon_it.spring_boot_with_mockito.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HomeController {
+
+    /**
+     * Test that the context is loaded.
+     */
+    @Test
+    public void runContext_contextLoads() {
+    }
+
+}
